b352fda86a4587f9099ecd63bf216c8f07a8d47e
shaffshalihin <113771313+shaffshalihin@users.noreply.github.com>
2022-09-25 19:09:26 +0800

    Delete praktikum2_pp_H071221093_no_1.py

--- praktikum 2/praktikum2_pp_H071221093_no_1.py
+++
@@ -1,6 +0,0 @@
-#program konversi angka ke huruf
-
-input_nilai = int(input('masukkan nilai:'))
-konversi = 'A' if input_nilai >= 90 else 'B' if input_nilai >= 80 else 'C' if input_nilai >= 70 else 'D' if input_nilai >= 60 else 'E' if input_nilai >= 40 else 'F'
-
-print('nilai', input_nilai, '=', konversi)
