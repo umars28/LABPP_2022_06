205a29868523ad3c774f7cf2eb1953edc4daace3
shaffshalihin <113771313+shaffshalihin@users.noreply.github.com>
2022-10-01 15:04:18 +0800

    TUGAS PRAKTIKUM 3

---
+++ H071221093_praktikum3/praktikum3_pp_H071221093_no_3.py
@@ -0,0 +1,13 @@
+bayar = int(input('bayaran anda:'))
+harga = int(input('harga barang:'))
+
+JenisUang = [100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100 ]
+kembalian = bayar - harga
+print('kembalian:', kembalian)
+
+for uang in JenisUang:
+    if uang > kembalian :
+        continue
+    BanyaknyaUang = int(kembalian / uang)
+    kembalian = kembalian - (uang * BanyaknyaUang)
+    print(BanyaknyaUang, 'uang Rp.', uang) 
