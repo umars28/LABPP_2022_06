af92c58b71375d522e2093f7cbabeb42b2d03100
shaffshalihin <shaff2004@gmail.com>
2022-10-09 22:52:00 +0800

    pengumpulan tugas praktikum 4

---
+++ H071221093/praktikum 4/praktikum4_pp_H071221093_no_1.py
@@ -0,0 +1,19 @@
+import numbers
+
+
+def sorting(numbers) :
+    for i in range(len(numbers)) :
+        print(i)
+        index_minimum = i
+        for j in range(i+1, len(numbers)) :
+            print(j)
+            if numbers[index_minimum] > numbers[j] :
+                index_minimum = j
+        if index_minimum != i :     
+            numbers[i], numbers[index_minimum] = numbers[index_minimum], numbers[i]
+
+
+numbers = list(map(int, input('angka:').split(' ')))
+print(f'sebelum diurutkan {numbers}')
+sorting(numbers)
+print(f'setelah diurutkan {numbers}')
